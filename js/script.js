84dfcaecb936596413575fd81902b3dd236d7c0e
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2022-12-18 15:18:23 +0200

    w

--- js/script.js
+++ js/script.js
@@ -11,3 +11,9 @@ window.onload = () => {
         window.scrollTo(0, 0)
     }
 }
+
+$(document).ready(function(){
+    $('.header_burger').click(function(event){
+        $('.header_burger,.header_menu').toggleClass('active');
+    });
+});
