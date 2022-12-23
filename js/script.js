5023d2754034cda20af46693cb274964e4d00e4c
Andrey3952 <a2006a2006@ukr.net>
2022-12-23 19:28:37 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-$(document).ready(function () {
+window.onload = () => {
     $('.header_burger').click(function (event) {
         $('.header_burger,.header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
 
     $('.list_menuLi').click(function (event) {
         $('.header_burger,.header_menu').removeClass('active');
         $('body').removeClass('lock');
     });
 
-});
 
-
-window.onload = () => {
     const scrollBtn = document.querySelector('.isShowBtn')
+
     window.onscroll = () => {
         if (window.scrollY > 1000) {
             scrollBtn.classList.remove('isShowBtn_hide');
