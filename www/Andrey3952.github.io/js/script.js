4e272a6779a5648b5e468c7cabcee26c5f95f223
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2023-01-16 13:42:55 +0200

    Add files via upload

---
+++ www/Andrey3952.github.io/js/script.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    $('.header_burger').click(function (event) {
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+
+    $('.list_menuLi').click(function (event) {
+        $('.header_burger,.header_menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
+
+
+    const scrollBtn = document.querySelector('.isShowBtn')
+
+    window.onscroll = () => {
+        if (window.scrollY > 1000) {
+            scrollBtn.classList.remove('isShowBtn_hide');
+        } else if (window.scrollY < 700) {
+            scrollBtn.classList.add('isShowBtn_hide');
+        }
+    }
+    scrollBtn.onclick = () => {
+        window.scrollTo(0, 0)
+    }
+}
+
