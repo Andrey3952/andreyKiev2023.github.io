491240cb046520693ad6ace807037a441674420a
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2023-01-22 23:48:01 +0200

    ф

--- js/script.js
+++ js/script.js
@@ -46,5 +46,21 @@ window.onload = () => {
     };
 
 
+
+
+
+
+    document.body.onload = function () {
+        setTimeout(function () {
+            var preloader = document.getElementById('page-preloader');
+            if (!preloader.classList.contains('done')) {
+                preloader.classList.add('done');
+            }
+        }, 10);
+    }
+
+
 };
+
+
 
