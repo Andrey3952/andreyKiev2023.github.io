dd4789c12088b19e47fafb5b3fe117d4e7fecf48
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2023-01-30 11:13:41 +0200

    и

--- js/script.js
+++ js/script.js
@@ -48,18 +48,6 @@ window.onload = () => {
 
 
 
-
-
-    document.body.onload = function () {
-        setTimeout(function () {
-            var preloader = document.getElementById('page-preloader');
-            if (!preloader.classList.contains('done')) {
-                preloader.classList.add('done');
-            }
-        }, 10);
-    }
-
-
 };
 
 
